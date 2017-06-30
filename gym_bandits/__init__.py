2482312f0fd1907fa497aec381610955b86b93f8
Thomas Lecat <tjl16@potoo01.doc.ic.ac.uk>
2017-06-30 18:15:55 +0100

    2017-06-30
    Modified version of JKCooper2's work to add the environments used in "Learning to RL"

---
+++ gym_bandits/__init__.py
@@ -0,0 +1,39 @@
+from gym.envs.registration import register
+
+from .bandit import BanditTenArmedRandomFixed
+from .bandit import BanditTenArmedRandomRandom
+from .bandit import BanditTenArmedGaussian
+from .bandit import BanditTenArmedUniformDistributedReward
+from .bandit import BanditTwoArmedDeterministicFixed
+from .bandit import BanditTwoArmedHighHighFixed
+from .bandit import BanditTwoArmedHighLowFixed
+from .bandit import BanditTwoArmedLowLowFixed
+from .bandit import BanditTwoArmedIndependantFixed
+from .bandit import BanditTwoArmedDependantUniform
+from .bandit import BanditTwoArmedDependantEasy
+from .bandit import BanditTwoArmedDependantMedium
+from .bandit import BanditTwoArmedDependantHard
+from .bandit import BanditElevenArmedWithIndex
+
+environments = [['BanditTenArmedRandomFixed', 'v0'],
+                ['BanditTenArmedRandomRandom', 'v0'],
+                ['BanditTenArmedGaussian', 'v0'],
+                ['BanditTenArmedUniformDistributedReward', 'v0'],
+                ['BanditTwoArmedDeterministicFixed', 'v0'],
+                ['BanditTwoArmedHighHighFixed', 'v0'],
+                ['BanditTwoArmedHighLowFixed', 'v0'],
+                ['BanditTwoArmedLowLowFixed', 'v0'],
+                ['BanditTwoArmedIndependantFixed', 'v0'],
+                ['BanditTwoArmedDependantUniform','v0'],
+                ['BanditTwoArmedDependantEasy','v0'],
+                ['BanditTwoArmedDependantMedium','v0'],
+                ['BanditTwoArmedDependantHard','v0'],
+                ['BanditElevenArmedWithIndex','v0']]
+
+for environment in environments:
+    register(
+        id='{}-{}'.format(environment[0], environment[1]),
+        entry_point='gym_bandits:{}'.format(environment[0]),
+        timestep_limit=1,
+        nondeterministic=True,
+    )
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gym_bandits',
+      version='0.01',
+      description='Gym User Env - Various N-Armed Bandit Problems',
+      url='https://github.com/jkcooper2/gym_bandits',
+      author='Jesse Cooper',
+      author_email='jesse_cooper@hotmail.com',
+      license='MIT License',
+      install_requires=['gym>=0.2.3'],
+)
